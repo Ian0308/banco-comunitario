8a48181f22d295052c01468e779b58b39e720d24
Davidosom <135185879+Davidosom@users.noreply.github.com>
2025-04-16 15:06:01 -0300

    Update Conta.ts

--- Classes/Conta.ts
+++ Classes/Conta.ts
@@ -2,10 +2,13 @@ import { IConta } from "../Interfaces/IConta"
 import { Cliente } from "./Cliente";
 
 export class Conta implements IConta{
-    constructor(public saldo: number,public tipo: "Corrente" | "Poupanca" = "Poupanca", cliente: Cliente){
-        this.saldo = saldo
-        this.tipo = tipo
-    }
+    constructor(
+    public saldo: number
+    public tipo: "corrente" | "poupanca"
+    public cliente: ICliente
+    public taxaJuros: number = 0.02
+    ){}
+}
 
     realizarDeposito(conta: IConta, quantia: number){
         if(conta.tipo = "Corrente"){
@@ -17,37 +20,56 @@ export class Conta implements IConta{
     }
 
     realizarSaque(conta: IConta, quantia: number){
-        if(conta.tipo = "Corrente"){
-            if(conta.saldo < quantia){
-                console.log(`Pode não fi, quer ficar devendo?`)    
-            }else{
+        if(conta.tipo == "Corrente"){
+            let limite = conta.saldo + 100
+            if(quantia <= limite){
                 conta.saldo -= quantia
+                console.log(`Saque de R$${quantia} realizado. Saldo: R$${conta.saldo}`)
+            }else{
+                console.log(`Pode não fi, quer ficar devendo?`)
             }
         }
-        if(conta.tipo = "Poupanca"){
-            if(conta.saldo < quantia){
-                console.log(`Pode não fi, quer ficar endividado?`)
-            }else{
+        if(conta.tipo == "Poupanca"){
+            if(quantia <= conta.sald){
                 conta.saldo -= quantia
+                console.log(`Saque de R$${quantia} realizado. Saldo: R$${conta.saldo}`)
+            }else{
+                console.log(`Pode não fi, quer ficar endividado?`)
             }
             
         }
     }
 
-    transferir(conta: IConta, quantia: number){
-        if(conta.tipo == "Corrente" && "Poupanca"){
-            if(quantia > conta.saldo){
-                console.log("É mais dinheiro do que você tem")
-            }else{
-                console.log("Pode transferir")
-            }
-        }else{
-            console.log("Não pode transferir")
-        }
+    transferir(quantia: number, destino: Conta): void {
+    if (
+      (conta.tipo == "corrente" && quantia <= conta.saldo + 100) ||
+      (conta.tipo == "poupanca" && quantia <= conta.saldo)
+    ) {
+      conta.sacar(quantia);
+      destino.depositar(quantia);
+      console.log(`Transferência de R$${quantia} para a conta ${destino.cliente}`);
+    } else {
+      console.log("Transferência não realizada.");
     }
+  }
+
+    verificarSaldo(): number {
+    console.log(`Saldo atual da conta: R$${conta.saldo}`);
+    return this.saldo;
+  }
 
     calcularTaxa(taxaJuros:number, conta: IConta){
         console.log(conta.saldo * taxaJuros) 
     }
+
+     calcularTaxa(): void {
+    if (conta.tipo == "poupanca") {
+      let juros = conta.saldo * conta.taxaJuros;
+      conta.saldo += juros;
+      console.log(`Juros aplicados.`);
+    } else {
+      console.log("Conta corrente não possui cálculo de juros.");
+    }
+  }
 
 }
