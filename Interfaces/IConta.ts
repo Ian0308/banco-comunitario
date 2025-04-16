7da2ca9a9484d44ae81df1bdde4ded329b0820eb
Davidosom <135185879+Davidosom@users.noreply.github.com>
2025-04-16 14:47:49 -0300

    Update IConta.ts

--- Interfaces/IConta.ts
+++ Interfaces/IConta.ts
@@ -2,4 +2,9 @@ export interface IConta{
     saldo: number
     tipo: "Corrente" | "Poupanca"
     conta: ICliente
+    depositar(valor: number): void;
+    sacar(valor: number): void;
+    transferir(valor: number, destino: Conta): void;
+    verificarSaldo(): number;
+    calcularTaxa?(): void;
 }
